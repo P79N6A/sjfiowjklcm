11aaabe76a2a31fa6922bb056b93ab3bd0fb533c
Mona Lisa <zhudavid80@gmail.com>
2018-12-20 01:33:47 +0800

    tyui

--- src/views/layouts/index.vue
+++ src/views/layouts/index.vue
@@ -4,42 +4,21 @@
       <div slot="header" class="cfx">
         <span>布局列表</span>
       </div>
-    <el-table
-      v-loading="listLoading"
-      :data="layoutList"
-      border
-      fit
-      highlight-current-row
-      style="width: 100%;"
-    >
-      <el-table-column :label="$t('table.id')" type="index" align="center" width="50"></el-table-column>
-      <el-table-column label="名称" prop="name"></el-table-column>
-      <el-table-column
-        :label="$t('table.actions')"
-        align="center"
-        width="230"
-        class-name="small-padding fixed-width"
-      >
-        <template slot-scope="scope">
-          <el-button-group>
-            <el-button
-              type="primary"
-              size="mini"
-              @click="handleUpdate(scope.row)"
-            >{{ $t('table.edit') }}</el-button>
-            <el-button
-              v-if="scope.row.status!='deleted'"
-              size="mini"
-              type="danger"
-              @click="handleDelete(scope.row)"
-            >
-              {{
-              $t('table.delete') }}
-            </el-button>
-          </el-button-group>
-        </template>
-      </el-table-column>
-    </el-table>
+      <el-table v-loading="listLoading" :data="layoutList" border fit highlight-current-row style="width: 100%;">
+        <el-table-column :label="$t('table.id')" type="index" align="center" width="50"></el-table-column>
+        <el-table-column label="名称" prop="name"></el-table-column>
+        <el-table-column :label="$t('table.actions')" align="center" width="230" class-name="small-padding fixed-width">
+          <template slot-scope="scope">
+            <el-button-group>
+              <el-button type="primary" size="mini" @click="handleUpdate(scope.row)">{{ $t('table.edit') }}</el-button>
+              <el-button v-if="scope.row.status!='deleted'" size="mini" type="danger" @click="handleDelete(scope.row)">
+                {{
+                $t('table.delete') }}
+              </el-button>
+            </el-button-group>
+          </template>
+        </el-table-column>
+      </el-table>
       <div class="bottom cfx">
         <el-button icon="el-icon-plus" type="primary" @click="handleCreate">添加布局</el-button>
       </div>
@@ -49,14 +28,14 @@
         <el-form-item label="*布局名称" prop="name">
           <el-input v-model="layoutTemp.name" />
         </el-form-item>
-        <el-form-item label="*布局终端" prop="name">
+        <el-form-item label="*布局终端" prop="device">
           <el-select v-model="layoutTemp.device" placeholder="请选择框架适用终端">
             <el-option label="PC端" value="pc"></el-option>
             <el-option label="MOBILE端" value="MOBILE"></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="*选择框架" prop="name">
-          <div class="frameList cfx">
+        <el-form-item label="*选择框架" prop="value">
+          <div class="frameList cfx" v-if="!layoutTemp.value">
             <div class="frames" v-for="(item,i) in frameList" :key="i" :class="{active:selectedFrameId==item._id}"
               @click="selectFrame(item)">
               <div class="frames-content">
@@ -75,14 +54,16 @@
               </div>
             </div>
           </div>
+          <div v-else>
+            <el-button @click="handleReset">重置</el-button>
+          </div>
         </el-form-item>
+
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogFormVisible = false">{{ $t('table.cancel') }}</el-button>
-        <el-button
-          type="primary"
-          @click="dialogStatus==='create'?createLayout():updateLayout()"
-        >{{ $t('table.confirm') }}</el-button>
+        <el-button type="primary" @click="dialogStatus==='create'?createLayout():updateLayout()">{{ $t('table.confirm')
+          }}</el-button>
       </div>
     </el-dialog>
   </div>
@@ -96,6 +77,7 @@
   import {
     getLayouts,
     addLayouts,
+    updateLayouts,
     deleteLayouts
   } from "@/api/layouts";
   export default {
@@ -112,14 +94,14 @@
           value: {},
           device: 'pc'
         },
-      
+
         dialogFormVisible: false,
         selectedFrameId: null,
-      dialogStatus: "",
-      textMap: {
-        update: "Edit",
-        create: "Create"
-      },
+        dialogStatus: "",
+        textMap: {
+          update: "Edit",
+          create: "Create"
+        },
       };
     },
     created() {
@@ -135,61 +117,87 @@
       },
       getLayouts() {
         getLayouts().then(res => {
-          this.layoutList=res.data
+          this.layoutList = res.data
         }).catch(err => {
 
         })
       },
-      createLayout(){
+      createLayout() {
         console.log(this.layoutTemp);
-        addLayouts(this.layoutTemp).then(res=>{
-            this.getLayouts();
-
-            this.$notify({
-              title: "成功",
-              message: "操作成功",
-              type: "success",
-              duration: 2000
-            });
-        }).catch(err=>{
+        addLayouts(this.layoutTemp).then(res => {
+          this.getLayouts();
+          this.dialogFormVisible = false;
+          this.$notify({
+            title: "成功",
+            message: "操作成功",
+            type: "success",
+            duration: 2000
+          });
+        }).catch(err => {
 
         })
       },
-      updateLayout(){
-        console.log('lksajf')
+      updateLayout(row) {
+        console.log('ghfj')
+        updateLayouts(this.layoutTemp).then(res => {
+                          this.$notify({
+                title: "成功",
+                message: "操作成功",
+                type: "success",
+                duration: 2000
+              });
+        }).catch(err => {
+        })
       },
       handleCreate() {
-        this.dialogStatus='create'
+        this.dialogStatus = 'create'
         this.dialogFormVisible = true;
         if (!this.frameList) {
           this.getFrames();
         }
       },
 
-      handleUpdate(){
-
+      handleUpdate(row) {
+        this.layoutTemp = row
+        this.dialogStatus = 'edit'
+        this.dialogFormVisible = true;
       },
-          handleDelete(row) {
-      this.$confirm("此操作将永久删除该数据, 是否继续?", "提示", {
-        confirmButtonText: "确定",
-        cancelButtonText: "取消",
-        type: "warning"
-      })
-        .then(() => {
-          deleteLayouts({ _id: row._id }).then(res => {
-            this.getLayouts();
-            this.$notify({
-              title: "成功",
-              message: "操作成功",
-              type: "success",
-              duration: 2000
+      handleDelete(row) {
+        this.$confirm("此操作将永久删除该数据, 是否继续?", "提示", {
+            confirmButtonText: "确定",
+            cancelButtonText: "取消",
+            type: "warning"
+          })
+          .then(() => {
+            deleteLayouts({
+              _id: row._id
+            }).then(res => {
+              this.getLayouts();
+              this.$notify({
+                title: "成功",
+                message: "操作成功",
+                type: "success",
+                duration: 2000
+              });
             });
+          })
+          .catch(err => {
+            console.log(err);
           });
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    },
+      },
+      handleReset() {
+        this.$confirm("此操作将清空原有布局, 是否继续?", "提示", {
+            confirmButtonText: "确定",
+            cancelButtonText: "取消",
+            type: "warning"
+          })
+          .then(() => {
+
+            this.layoutTemp.value = null;
+            this.selectedFrameId=null;
+            this.getFrames()
+          })
+      },
       selectFrame(data) {
         console.log("lksfjlkasjf")
         this.layoutTemp.value = data.value;
@@ -218,7 +226,7 @@
         &.active {
           .frames-content {
             border-color: red;
-            box-shadow:0 0 4px 2px #ccc;
+            box-shadow: 0 0 4px 2px #ccc;
           }
         }
 
@@ -233,7 +241,7 @@
           border: solid 1px #ccc;
           border-radius: 6px;
           font-size: 12px;
-          transition:all .6s;
+          transition: all .6s;
         }
 
         .cols {
