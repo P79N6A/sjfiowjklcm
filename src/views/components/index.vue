6df61d5a6977be3fdb6e4963d6ca363a6fba9445
Mona Lisa <zhudavid80@gmail.com>
2018-12-27 00:24:58 +0800

    commit

--- src/views/components/index.vue
+++ src/views/components/index.vue
@@ -1,56 +1,6 @@
 <template>
-  <div class="components-container">
-    <el-card class="box-card">
-      <div slot="header" class="cfx">
-        <span>组件列表</span>
-          <!-- <el-input placeholder="请输入内容" v-model="input5" style="width:500px;" size="midium">
-            <el-select v-model="select" slot="prepend" placeholder="请选择">
-              <el-option label="餐厅名" value="1"></el-option>
-              <el-option label="订单号" value="2"></el-option>
-              <el-option label="用户电话" value="3"></el-option>
-            </el-select>
-            <el-button slot="append" icon="el-icon-search"></el-button>
-          </el-input> -->
-      </div>
-      <el-container>
-        <el-aside width="200px">
-          <el-menu default-active="2" class="el-menu-vertical-demo">
-            <el-submenu index="1">
-              <template slot="title">
-                <i class="el-icon-location"></i>
-                <span>导航一</span>
-              </template>
-              <el-menu-item-group>
-                <template slot="title">分组一</template>
-                <el-menu-item index="1-1">选项1</el-menu-item>
-                <el-menu-item index="1-2">选项2</el-menu-item>
-              </el-menu-item-group>
-              <el-menu-item-group title="分组2">
-                <el-menu-item index="1-3">选项3</el-menu-item>
-              </el-menu-item-group>
-              <el-submenu index="1-4">
-                <template slot="title">选项4</template>
-                <el-menu-item index="1-4-1">选项1</el-menu-item>
-              </el-submenu>
-            </el-submenu>
-            <el-menu-item index="2">
-              <i class="el-icon-menu"></i>
-              <span slot="title">导航二</span>
-            </el-menu-item>
-            <el-menu-item index="3" disabled>
-              <i class="el-icon-document"></i>
-              <span slot="title">导航三</span>
-            </el-menu-item>
-            <el-menu-item index="4">
-              <i class="el-icon-setting"></i>
-              <span slot="title">导航四</span>
-            </el-menu-item>
-          </el-menu>
-        </el-aside>
-        <el-main>Main</el-main>
-      </el-container>
-    </el-card>
-    
+  <div class="code-container">
+        <iframe src="/static/codePend/index.html" style="border:none;width:100%;height:100vh;"></iframe>
   </div>
 </template>
 
@@ -93,6 +43,6 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-.components-container {
+.code-container {
 }
 </style>
